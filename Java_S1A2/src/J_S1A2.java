0e867df698907d086e832692e0a0e9de0c6598bd
BrijeshParambikadan <bbavittom@gmail.com>
2016-10-11 22:11:25 +0530

    submitted javaS1A2 project

---
+++ Java_S1A2/src/J_S1A2.java
@@ -0,0 +1,28 @@
+
+public class J_S1A2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=20;
+int b=10;
+
+
+System.out.print("Initial value of a is "+a+" & b is "+b);
+System.out.print("\n");
+
+b= a-- - --a;
+int c=a--;
+int d=a>>2;
+int e=a&b;
+
+System.out.print("\n");
+System.out.print("Value of (b=a-- - --a) is "+b);
+System.out.print("\n");
+System.out.print("Value of (c=a--) is "+c);
+System.out.print("\n");
+System.out.print("Value of (d=a>>2) is "+d);
+System.out.print("\n");
+System.out.print("Value of (e=a & b) is "+e);
+	}
+
+}
